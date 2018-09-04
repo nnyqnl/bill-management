f8a72d05da2e9a853c0691a37e4d9576d81440e7
nnyqnl <15538103935@163.com>
2018-09-04 18:20:11 +0800

    0904

--- src/main/java/com/doudou/bill/controller/UserController.java
+++ src/main/java/com/doudou/bill/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.doudou.bill.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.doudou.bill.common.constant.ResultCode;
 import com.doudou.bill.common.dto.BillResult;
 import com.doudou.bill.common.dto.UserDto;
 import com.doudou.bill.service.UserService;
 import com.doudou.bill.orm.model.User;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -63,6 +66,20 @@ public class UserController {
         BillResult br = userService.userList();
 
         return br;
+    }
+
+    public static void main(String[] args) {
+        String aa="{ 'resp': { 'respCode': 0, 'callOut': { 'callId': 'api001000227d01535592926011jm8M2', 'createTime': '20180830093526' } }, 'callName': 'ddddd'}";
+        JSONObject jsonObject = JSONObject.parseObject(aa);
+        JSONObject resp = jsonObject.getJSONObject("resp");
+        JSONObject callOut = resp.getJSONObject("callOut");
+        Object callId1 = callOut.get("callId");
+
+//        JSONObject jsonObject = JSON.parseObject(aa);
+        Object callId = jsonObject.getJSONObject("resp").getJSONObject("callOut").get("callId");
+        Object respCode = jsonObject.getJSONObject("resp").get("respCode");
+
+        System.out.println((String)callId);
     }
 
 }
